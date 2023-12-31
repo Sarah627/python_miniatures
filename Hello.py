6253cd6250996eab67ad03063e9f4bb861d2c158
Sarah Sameh <sarahsameh627@gmail.com>
2023-12-31 18:28:13 +0200

    The very first file in python, just writing hello world on terminal

---
+++ Hello.py
@@ -0,0 +1,7 @@
+print("hello,world")
+# just the very first code line in python, printing
+# hello, world on the console, we'are just using the print function print()
+# if we wanna write text so insert this text in a double quotes
+# in the parenthesis of the print function, and that's it!
+
+# to interpret the code in the terminal, type python filename.py
