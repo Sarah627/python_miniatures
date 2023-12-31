0a6e9f2593532996fcf968e4de14396c3d2a159e
Sarah Sameh <sarahsameh627@gmail.com>
2023-12-31 23:07:55 +0200

    In this file we enhanced the key idea of Hello.py file but using functions

---
+++ HelloEnhanced.py
@@ -0,0 +1,22 @@
+# In this file we are going to deal basically with functions
+# The main function here tells this is the main part of the program
+# and thus we can list the functions definition underneath it
+def main():
+    hello()
+    name = input("Please enter your name:").strip().title()
+    hello(name)
+    number = int(input("enter a number you want to format: "))
+    print(formatNumber(number))
+
+
+def hello(name="world!"):
+    print(f"Hello, {name}")
+
+# function with a return value
+
+
+def formatNumber(number):
+    return f"{number:,}"
+
+
+main()
