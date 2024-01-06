56f3d933b3d9633a278eaa2e84252ca7adcf34c7
Sarah Sameh <sarahsameh627@gmail.com>
2024-01-06 21:42:44 +0200

    trying some tricks in python makes numbers more readable

---
+++ basics/tricksWithNumbers.py
@@ -0,0 +1,7 @@
+# We can make numbers more readable
+# While coding:
+fnumber = 1000_000_000_000
+snumber = 233_454_584
+simple_op = fnumber - snumber
+# While printing to console usnig format string
+print(f'{simple_op:,}')  # you can replace comma with underscore
