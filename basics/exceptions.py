2c5c7a88a55d2721f485ee26698e58ff3a7ce76e
Sarah Sameh <sarahsameh627@gmail.com>
2024-02-09 20:49:40 +0200

    the logic of the loop changed, as teh break keyword moved to try block, as if any error happened the code will jump into exception line directly without any need to else statement

--- basics/exceptions.py
+++ basics/exceptions.py
@@ -1,8 +1,6 @@
 while True:
     try:
         x = int(input("What's x?: "))
+        break
     except ValueError:
         print("X is not an integer, please enter integer")
-    else:
-        print(f"x is {x}")
-        break
