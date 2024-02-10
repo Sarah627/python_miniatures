4ea1a324ae35f6993e12d938f93207cb4c71d9d6
Sarah Sameh <sarahsameh627@gmail.com>
2024-02-10 02:49:48 +0200

    A simple user defined library is made to be imported abd used in other files. it contains two simple functions [hello, goodbye] which simply print hello and goodbye sentences associated with the passed string value __name__ is used to prevent main function to be called when the module is imported in othe files

---
+++ basics/saying.py
@@ -0,0 +1,15 @@
+def main():
+    hello("Sarah")
+    goodbye("Sarah")
+
+
+def hello(name):
+    print(f"Hello, {name}")
+
+
+def goodbye(name):
+    print(f"GoodBye, {name}")
+
+
+if __name__ == "__main__":
+    main()
