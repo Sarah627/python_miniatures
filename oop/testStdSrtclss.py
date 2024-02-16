2266d144b718ef3d3086712c9868bc1765b8be61
Sarah Sameh <sarahsameh627@gmail.com>
2024-02-16 23:57:12 +0200

    An instance of the super class -Wizard- is invoked

--- oop/testStdSrtclss.py
+++ oop/testStdSrtclss.py
@@ -1,16 +1,20 @@
 from students import Student
+from wizard import Wizard
 from sortinghat import Hat
 
 
 def main():
+    # Super Class
+    wizard = Wizard("Albus").define_wiz()
     Hat.sort()
     try:
+        # Subclass of Suprt Class "Wizart"
         student = Student.get_student()
         print(student)
     except ValueError:
-        print("Missing Name or House")
+        print("Missing Name or House or wrong house name")
+
+
     # student.house = "Number Four, Privet Drive "
-
-
 if __name__ == "__main__":
     main()
