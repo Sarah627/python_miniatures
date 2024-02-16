670dc4686e48f94f9ff32b514c2e105f69774d42
Sarah Sameh <sarahsameh627@gmail.com>
2024-02-16 17:07:52 +0200

    __str__ function is added to Student class

--- oop/students.py
+++ oop/students.py
@@ -1,17 +1,29 @@
 class Student:
-    ...
+    # applying abstraction and encapsulation
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing Name")
+        if house not in ["Gryffindor", "Syltherin", "Hafflbuff", "Ravenclaw"]:
+            raise ValueError("Invalid House")
+        self.name = name  # attributes
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
-    student = Student()
-    student.name = input("What's your name? ")
-    student.house = input("What's your house? ")
-    return student
+    name = input("What's your name? ")
+    house = input("What's your house? ")
+    try:
+        return Student(name, house)
+    except ValueError:
+        pass
 
 
 if __name__ == "__main__":
