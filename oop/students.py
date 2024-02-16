51efd32597b54386d3ce91f01ce68ccdc593cbbb
Sarah Sameh <sarahsameh627@gmail.com>
2024-02-16 20:00:16 +0200

    patronus argument is addde to the inializer with a defualt value, a setter and getter are added, @property is used to add more defensive functionality to the setter and getter

--- oop/students.py
+++ oop/students.py
@@ -1,27 +1,59 @@
 class Student:
     # applying abstraction and encapsulation
-    def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing Name")
-        if house not in ["Gryffindor", "Syltherin", "Hafflbuff", "Ravenclaw"]:
-            raise ValueError("Invalid House")
+    def __init__(self, name, house, patronus=None):
         self.name = name  # attributes
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
-        return f"{self.name} from {self.house}"
+        return f"{self.name} from {self.house} and your patronus is {self.charm()}"
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "🍕"
+            case "Otter":
+                return "🍰"
+            case "Doe":
+                return "\nLily, After all this time? "
+            case  _:
+                return "🌿"
+    # Getter and Setter
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name
+
+    @property
+    # Getter
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Syltherin", "Hafflbuff", "Ravenclaw"]:
+            raise ValueError("Invalid House")
+        self._house = house
 
 
 def main():
     student = get_student()
+    # student.house = "Number Four, Privet Drive "
     print(student)
 
 
 def get_student():
     name = input("What's your name? ")
     house = input("What's your house? ")
+    patronus = input("Enter your Patronus: ")
     try:
-        return Student(name, house)
+        return Student(name, house, patronus)
     except ValueError:
         pass
 
